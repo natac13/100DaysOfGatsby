1502791f4b1202e558d42cb5e31f5a428c20c6ba
Sean Campbell <sean.campbell13@gmail.com>
2021-01-01 17:40:40 -0500

    Fixw link in prod

--- src/pages/locations/index.tsx
+++ src/pages/locations/index.tsx
@@ -1,9 +1,9 @@
-import { Typography } from '@material-ui/core'
 import { graphql, PageProps } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import { LocationsQuery } from '../../types/generated-gatsby'
 import { Link } from 'gatsby-material-ui-components'
+import { IS_DEV } from '../../constants'
 
 interface Props extends PageProps {
   data: LocationsQuery
@@ -14,7 +14,12 @@ const Locations: React.FC<Props> = (props: Props) => {
   return (
     <Layout>
       {data.allContentfulLocation.nodes.map((location) => (
-        <Link to={`locations/${location.slug}`}>{location.city}</Link>
+        <Link
+          key={location.id}
+          to={`${IS_DEV ? 'locations/' : ''}${location.slug}`}
+        >
+          {location.city}
+        </Link>
       ))}
     </Layout>
   )
